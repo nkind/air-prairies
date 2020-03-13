604fa6e4b5913b74c3db0237bce02e78778e3a1c
Jordan Liao (yol474) <yol474@mail.usask.ca>
2020-03-13 16:45:47 -0600

    Replace app.js

--- app.js
+++ app.js
@@ -1,136 +1,321 @@
+var nodemailer = require('nodemailer');
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 
-const express = require("express");
-var bodyParser = require('body-parser')
-var urlencodedParser = bodyParser.urlencoded({extended: false})
-const app = express();
-var server = require('http').Server(app);
-fs = require('fs')
-server.listen(12345)
-
-
-
-
-/* connecting to the database */
 const sqlite3 = require('sqlite3').verbose()
 let db = new sqlite3.Database('Airprairiess.db3', (err) => {
     if (err){
         return console.error("unable to connect");
     }
-    console.log('connected to database');
+    console.log('connected to database')
+})
+
+
+// variable use to store the results of qurries 
+var savedresults = [];
+var flightinfo=[];
+
+
+app.get("/searchResults", function(req, res){
+    console.log(savedresults)
+  
+      let big_sql = 
+  
+      ` SELECT *
+      FROM OneWayFlights
+      WHERE origin="${savedresults[0].origin}" AND destination=(
+      
+          SELECT F1.destination AS stop
+          FROM OneWayFlights F1
+          JOIN OneWayFlights F2 ON F2.origin = F1.destination
+          WHERE F1.origin="${savedresults[0].origin}" AND F2.destination="${savedresults[0].destination}"
+      )
+      
+      UNION 
+      
+      SELECT *
+      FROM OneWayFlights
+      WHERE origin=(
+      
+          SELECT F1.destination AS stop
+          FROM OneWayFlights F1
+          JOIN OneWayFlights F2 ON F2.origin = F1.destination
+          WHERE F1.origin="${savedresults[0].origin}" AND F2.destination="${savedresults[0].destination}"
+      ) AND destination="${savedresults[0].destination}"
+  
+      `
+   
+      let small_sql = `SELECT * from OneWayFlights WHERE origin = '${savedresults[0].origin}' and destination = '${savedresults[0].destination}' Order By origin ASC`
+      
+  
+      /* runing the smaller query to check if there is a direct flight
+        from the origin to the destination
+      */
+      db.all(small_sql, function(err,row){
+          if (err) throw err;
+
+          // send the flight info to the search reuslt page
+          if (row.length != 0){
+              flightinfo.push(row)
+  
+              res.render("searchResults", {savedresults:row})
+              savedresults.push(row)
+              savedresults = []
+  
+              
+          }
+          /*no direct flight so, the bigger query is run to check 
+          for connections*/
+          else{
+              biggerquery()
+          }
+  
+      });
+  
+      function biggerquery(){
+  
+          db.all(big_sql, function(err,row){
+              if (err) throw err;
+              
+              console.log("reached")
+
+              // send the flight info to the search reuslt page
+              if (row.length != 0){
+                  flightinfo.push(row)
+      
+                  res.render("searchResults", {savedresults:row})
+                  savedresults.push(row)
+                  savedresults = []
+  
+  
+                 
+              }
+            
+              // no connection found redirect the user back to the search page
+              else{
+              
+                  res.redirect("/search")
+              }
+              
+      
+          });
+  
+      }
+  
+
+  
+  });
+
+ 
+
+    
+
+/* Query to fill in the dropdown menu in the 
+main page with the locations from the database*/
+app.get("/search", function(req, res){
+
+    let sql = `SELECT DISTINCT origin from OneWayFlights Order By origin ASC`
+
+    // send the locations to the search reuslt page
+    db.all(sql,function(err,row){
+        if (err) throw err;
+       res.render('search', { oneWayFlights: row });
+     })
+
+    
+
+
+});
+
+/* get the user information from the form in the 
+search page and add them into the savedresult array.*/
+app.post("/searchResults", function(req, res){
+    // the input information from the search page
+    var tripchoice = req.body.tripchoice;
+    var origin = req.body.origin;
+    var destination = req.body.destination;
+    var aresult = {tripchoice:tripchoice, origin:origin, destination:destination};
+    savedresults.push(aresult);
+    // dislay results here
+    res.redirect("/searchResults");
+
 });
 
 
 
- /* Displaying information of User  table*/
+// render the about page
+app.get("/aboutpage", function(req, res){
+    res.render("aboutpage");
 
 
-let sql = `SELECT DISTINCT origin dep FROM OneWayFlights ORDER BY dep`; // Querying from the database
+});
 
 
-var a_array = []
-db.all(sql,[],(err, rows) => {
+app.get("/info", function(req, res){
 
-    if (err){
-        throw err;
+    res.render("info");
+
+
+});
+
+
+var paymentinfo=[]
+/* adds the userinput from the information page 
+to the paymentingo array*/
+app.post("/payment", function(req, res){
+    var name = req.body.name;
+    var age = req.body.age;
+    var gender = req.body.gender;
+    var nationality = req.body.nationality;
+    var phone = req.body.phone;
+    var address1 = req.body.address1;
+    var address2 = req.body.address2;
+    var country = req.body.country;
+    var province = req.body.province;
+    var postalcode = req.body.postalcode;
+    var email = req.body.email;
+    var flightnumber = req.body.flightnumber;
+
+    var save_result = {name:name, age:age, gender:gender, nationality:nationality, phone:phone, address1:address1, address2:address2, country:country, province:province, postalcode:postalcode, email:email, flightnumber:flightnumber}
+    
+    // push info into payment array
+    paymentinfo.push(save_result);
+    // render the payment page.
+    res.redirect("/payment")
+})
+
+
+
+app.get("/payment", function(req, res){
+    // Inserting the customerinformation into the Bookings table in the database.
+
+        db.run(`INSERT INTO Bookings (
+            Name, 
+            age, 
+            gender, 
+            Nationality, 
+            [Phone Number], 
+            Address1, 
+            Address2, 
+            Country, 
+            [Province/Territory], 
+            [Postal Code],
+            Email, 
+            flightnumber) 
+            VALUES(
+                '${paymentinfo[0].name}', 
+                '${paymentinfo[0].age}',
+                '${paymentinfo[0].gender}',
+                '${paymentinfo[0].nationality}',
+                '${paymentinfo[0].phone}',
+                '${paymentinfo[0].address1}',
+                '${paymentinfo[0].address2}',
+                '${paymentinfo[0].country}',
+                '${paymentinfo[0].province}',
+                '${paymentinfo[0].postalcode}',
+                '${paymentinfo[0].email}',
+                '${paymentinfo[0].flightnumber}'
+                )`
+        )
+    
+    res.render('payment');
+
+});
+
+
+app.post("/reserved", function(req, res){
+    let sql = `SELECT MAX(bookingId) as save from bookings`
+
+    var book_ref 
+    console.log("reached")
+    console.log(flightinfo)
+    console.log(ref_info)
+    db.all(sql,function(err,row){
+        book_ref = row
+        console.log(book_ref[0].save)
+
+        
+     res.render("reserved");
+
+     var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // use SSL
+    
+    auth:{
+        user: 'airprairies@gmail.com',
+        pass: 'ajnw#0206'
+    }
+})
+
+var mailOptions = {
+    from: 'jordan.liao12@gmail.com',
+    to: `${paymentinfo[0].email},`,
+    subject:'confirm',
+    text: `your booking has now been confirmed. Your booking reference is ${book_ref[0].save} and your flight number is ${flightinfo.flightnumber} Thank you for flying with us` 
+};
+
+console.log("hello")
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}) 
+
+});
+
+app.get('/reserved', function(req, res){
+
+})
+
+
+// render the reference page.
+
+
+
+var ref_info = []
+
+app.get('/reference', function(req, res){
+    res.render('reference')
+})
+
+app.post('/reference', function(req, res){
+    var flightnumber = req.body.flightnumber
+    var bookingid = req.body.bookingid
+    var save = {flightnumber:flightnumber, bookingid:bookingid}
+
+    ref_info.push(save);   
+    res.redirect("/result")
+})
+
+app.get('/result', function(req, res){
+    
+    console.log(ref_info[0].bookingid)
+    if (ref_info != []){
+    let sql =  `SELECT OneWayFlights.* FROM OneWayFlights 
+                JOIN Bookings on OneWayFlights.flightnumber = Bookings.flightnumber
+                WHERE Bookings.bookingid = '${ref_info[0].bookingid}' `
+    
+    db.all(sql,function(err,row){
+        console.log(row)
+        if (err) throw err;
+        res.render('result', {ref_info:row });
+        })
     }
 
-    // console.log("db.all")
-    rows.forEach((row)=>{
-        // console.log(row.id, row.age)
-        a_array.push(row.dep)
-   
-
-    });
-
-    app.get('/User.html', function (req, res){
-
-        res.send(
-   
-           `<!DOCTYPE html>
-           <html>
-                <body>
-                   
-
-                   <form >
-
-                    <label for="name">departure location:</label>
-                    <select id = "activity">
-                        <option value = ""> ------ Select -------</option>
-
-                    </select>
-
-                    <br>
-
-                    <!-- ######### Arrival section ##### -->
-
-                    <label for="name">destination location:</label>
-                    <select id = "activity1" >
-                        <option value = ""> ------ Select -------</option>
+})
 
 
-                    </select>
 
-                    <br>
-                    <label for="party">Choose your departure_date:
-                        <input type="date" name="party" min="2020-01-01" max="2022-04-30">
-                    </label>
-
-                    <br>
-                    <input class = "button" id = "sub" type ="button" name = "Subbmit" value = "Subbmit">
-
-                </form>
-
-
-                <script type="text/javascript">
-
-                        string1 = '${a_array}'.split(",");
-                        var select = document.getElementById("activity");
-               
-                        for (var i = 0; i < string1.length; i++){
-
-                            var opt = string1[i];
-                            var el = document.createElement("option")
-                            el.textContent = opt;
-                            el.value = opt;
-                            select.appendChild(el);
-                        }
-
-                    // reading from the dropdown menu
-                       
-                        var sub = document.getElementById("activity").onclick = function(){displayresult()}
-
-                        function displayresult(){
-
-                       
-                        var x = document.getElementById("activity")
-                        var save = x.options["selectedIndex"]
-                        var result = document.getElementById("activity").options[save].text
-                        console.log(result)
-}
-
-                    </script>
-
-                </body>
-            <html>`    
-
-        )
-        
-    })
-       
+app.listen(2020, function(){
+    console.log("The server has started");
 });
-
-
- app.get('/Confirmation.html', function (req, res){
-     
-     console.log(req.body)
-     console.log("help")
-     res.render('Confirmation-success', {data: req.body});
-     
- })
-
-
-db.close();
-
-
 
